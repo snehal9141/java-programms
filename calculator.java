d722c9dce1fb30f583e2f56655b77a587faec7e2
snehal gorde <88132661+snehal9141@users.noreply.github.com>
2023-02-26 18:23:11 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    char operator;
+	    double number1,number2,result;
+	    
+	    Scanner sc=new Scanner(System.in);
+	    
+		System.out.println("Choose an operator: +,-,*,/");
+		operator = sc.next().charAt(0);
+		
+		System.out.println("Enter the first number");
+		number1 = sc.nextDouble();
+		
+		System.out.println("Enter the second number");
+		number2 = sc.nextDouble();
+		
+		switch(operator){
+		    case '+':
+		        result = number1+number2;
+		        System.out.println(number1+ "+"+ number2+ "="+result);
+		        break;
+		        
+		        case '-':
+		            result = number1-number2;
+		            System.out.println(number1+ "-"+number2+ "="+result);
+		            break;
+		            
+		            case '*':
+		                result = number1 *number2;
+		                System.out.println(number1+ "*"+number2+ "="+result);
+		                break;
+		                
+		                case '/':
+		                    result = number1 / number2;
+		                    System.out.println(number1+ "/"+number2+ "="+result);
+		                    break;
+		}
+		sc.close();
+	}
+}
